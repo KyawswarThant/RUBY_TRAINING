d4ae833d5ec0f4746574f4091beea541939450e3
kyawswar <kyawswar.thant25@gmail.com>
2021-12-15 11:42:36 +0630

    [master] update ruby tutorial 2

--- tutorials/tutorial_02/index.rb
+++ tutorials/tutorial_02/index.rb
@@ -1,6 +1,6 @@
 star = "*"
 space = 10
-decre = 4
+decre = 4   #set decrement value for star when it reach 11"
 
 22.times do |i|
 
@@ -9,6 +9,7 @@ decre = 4
     if i < 12
 
       space.times do
+        # add space infront of star
         print " "
       end
 
@@ -16,13 +17,14 @@ decre = 4
         print "#{star} "
       end
 
-      puts "\n\n"
+      puts "\n\n"   # add 2 line after every line"
       space -= 2 unless i == 11
       
     else
       space += 2
 
       space.times do
+        # add space infront of star
         print " "
       end
 
@@ -31,7 +33,7 @@ decre = 4
       end
 
       puts "\n\n"
-      decre += 4
+      decre += 4  # plus decrement value to 4 to adjust star count"
     end
 
   end
