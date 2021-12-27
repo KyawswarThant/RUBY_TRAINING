872113397db83776b7e3c4b4cb34b376b405793f
kyawswar <kyawswar.thant25@gmail.com>
2021-12-27 15:55:53 +0630

    [feature/tutorial_07] fix feedback

--- tutorials/tutorial_07/config/routes.rb
+++ tutorials/tutorial_07/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "upload_image#index"
-  get 'upload_image/index'
-  post 'handle_upload', to: "upload_image#handle_upload"
+  get "upload_image/index"
+  get "handle_upload", to: "upload_image#handle_upload"
+  post "handle_upload", to: "upload_image#handle_upload"
 end
